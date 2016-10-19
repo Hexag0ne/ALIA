ade85dba6efcc272b601a8950c94341450345933
MSouka <soukaina.majdi@insa-lyon.fr>
2016-10-19 10:53:19 +0200

    Add files via upload

--- puissance4.pl
+++ puissance4.pl
@@ -1,22 +1,9 @@
 :-module( puissance4, [nextPlayer/2, displayBoard/1, gameOver/3]).
 
-%Predicat qui permet de dessiner un élément
-displayElem(H) :- H = a, write('o'),  write(' | ').
-displayElem(H) :- H = b, write('x'),  write(' | ').
-displayElem(H) :- H \= a, H \= b, write(' '),  write(' | ').
-
-%Predicat qui permet de dessiner une ligne
-displayLine(_,8,_).
-displayLine(Grille,X,Y) :-  nth1(X,Grille,Column), nth1(Y,Column,Elem), displayElem(Elem), Xa is X+1, displayLine(Grille,Xa,Y), !. 
-displayLine(Grille,X,Y) :-  displayElem(c) , Xa is X+1, displayLine(Grille,Xa,Y), !. 
-
-%Predicat qui permet de dessiner toutes les colonnes. Il dessine les lignes une par une et redessine par recursion
-displayCol(_,_,0).
-displayCol(Grille,X,Y) :- displayLine(Grille,X,Y), nl, Ya is Y - 1 , displayCol(Grille,X,Ya), !.
-
 %%% DisplayBoard
-% Fonction permettant d'afficher la grille de jeu
-displayBoard(Grille) :- X is 1, Y is 7, displayCol(Grille,X,Y). 
+% Fonction permettant d'afficher la grille de jeu contenue dans
+displayBoard(Grille):-writeln('-------').
+
 
 %%% GameOver(Grille, Colonne, Winner)
 % Cette règle doit être vraie si un joueur gagne ou si le jeu se termine en égalité.
@@ -24,9 +11,16 @@ displayBoard(Grille) :- X is 1, Y is 7, displayCol(Grille,X,Y).
 % La grille est la grille de jeu (sept listes de sept éléments)
 % La colonne est la colonne dans laquelle le dernier pion a été placé
 % Winner prends la valeur du joueur qui gagne la partie ou 'draw' s'il y a égalité.
-gameOver(_, X, Winner):- Winner = 'Caramba', !.
+
+
+
+
+gameOver(_, _, Winner):- Winner = 'Caramba', !.
+gameOver(Grille, _, 'Draw'):- isfull(Grille).
+
+isfull([H|T]|) :- nth0(7,H,Y),nonvar(Y), isfull(T).
 
 %%%nextPlayer(Player, Nextplayer)
 %Si le joueur courant est l'humain, le suivant est la machine et inversement !
 nextPlayer('H', 'M').
-nextPlayer('M', 'H').
+nextPlayer('M','H').
