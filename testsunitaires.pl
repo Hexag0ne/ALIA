eae03e124645a0d0d4fbe1acb6d97d0d8eb166cb
patrick <patrick.finnerty@insa-lyon.fr>
2016-10-28 12:46:17 +0200

    Ajouté d'autres tests. Rajouté un retour à la ligne après chaque test.

--- testsunitaires.pl
+++ testsunitaires.pl
@@ -1,12 +1,28 @@
 % Fichier contenant les tests unitaires fait sur nos différents morceaux de codes
 
 :- use_module(puissance4).
+:- use_module(util).
 
 %Lance touts les tests.
 %Si le test passe, affiche OK ; KO dans le cas contraire.
-tests :- write('joueurSuivant : '),(testJoueurSuivant -> write('OK') ; write('KO')).
+tests :-
+	write('joueurSuivant:'),(testJoueurSuivant -> write('OK') ; write('KO')),nl,
+	write('ajouterCase  :'),(testAjouterCase -> write('OK') ; write('KO')),nl,
+	write('valeurGrille :'),(testValeurGrille -> write('OK') ; write('KO')),nl.
+				
+%%%% Tests du module Util
+%Test de ajouterCase/3
+testAjouterCase :-
+	ajouterCase('x',[],['x']),
+	not(ajouterCase('o',[],['x'])),
+	ajouterCase('x',['o','x'],['o','x','x']).
 
-%Test des règles joueurSuivant
+%Test de valeurGrille/4
+testValeurGrille :-
+	valeurGrille([_,[n,'x'],_,_,_,_,_,_,_],1,1,'x').
+
+%%%% Tests du module puissance4
+%Test de joueurSuivant/2
 testJoueurSuivant :-
 	joueurSuivant('x','o'),
 	joueurSuivant('o','x'),
