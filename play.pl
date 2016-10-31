2a96641bf14ee7c68e1952f47731605760fd22a8
patrick <patrick.finnerty@insa-lyon.fr>
2016-10-31 17:48:28 +0100

    Ajouté coupGagnant. Les tests sont en cours de préparation

--- play.pl
+++ play.pl
@@ -2,7 +2,10 @@
 % Ce fichier contient les règles d'intelligence artificielle permettant à la fois à la machine et au joueur 'humain' de décider de leurs coups.
 
 %%% Export du module play et de ses prédicats
-:-module('play', [joue/3]).
+:-module('play', [joue/3,coupGagnant/3]).
+
+:-use_module('puissance4.pl').
+:-use_module('util.pl').
 
 %Demande au joueur de choisir une colonne où placer son pion. Joue a deux règles, une quand joueur est l'humain ('x'), une quand joueur est machine ('o')
 joue('x', _, ColonneJoue):-
@@ -10,3 +13,9 @@ joue('x', _, ColonneJoue):-
 
 joue('o', _, ColonneJoue):-
     write('Choisissez une colonne [1-7]: '), read(ColonneJoue), integer(ColonneJoue), ColonneJoue < 8, ColonneJoue > 0.
+
+
+%Renvoie vrai dans le cas où si le joueur place son pion dans la colonne ColonneJoue alors celui-ci remporte immédiatement la partie
+coupGagnant(Grille,ColonneJoue,Joueur) :-
+	jouerCoup(Grille,ColonneJoue,Joueur,Z),
+	gameOver(Z,ColonneJoue,Joueur).
