04204032f4ec3865fd4829448786a3782c8ee85c
yMorakkam <yassine.kam@gmail.com>
2016-10-31 12:05:40 +0100

    Update play.pl

--- play.pl
+++ play.pl
@@ -6,7 +6,7 @@
 
 %Demande au joueur de choisir une colonne où placer son pion. Joue a deux règles, une quand joueur est l'humain ('x'), une quand joueur est machine ('o')
 joue('x', _, ColonneJoue):-
-    write('Choisissez une colonne [1-7]: '), read(ColonneJoue), integer(ColonneJoue), ColonneJoue < 7, ColonneJoue > 0.
+    write('Choisissez une colonne [1-7]: '), read(ColonneJoue), integer(ColonneJoue), ColonneJoue < 8, ColonneJoue > 0.
 
 joue('o', _, ColonneJoue):-
-    write('Choisissez une colonne [1-7]: '), read(ColonneJoue), integer(ColonneJoue), ColonneJoue < 7, ColonneJoue > 0.
+    write('Choisissez une colonne [1-7]: '), read(ColonneJoue), integer(ColonneJoue), ColonneJoue < 8, ColonneJoue > 0.
